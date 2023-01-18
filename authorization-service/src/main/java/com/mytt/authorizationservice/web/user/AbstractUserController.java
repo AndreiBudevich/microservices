6a5b1521d1ebfdc1c0e6528d7c1564edc100b6e3
Andrei Budevich <budevichac01@gmail.com>
2023-01-18 19:29:18 +0300

    create AbstractUserController

---
+++ authorization-service/src/main/java/com/mytt/authorizationservice/web/user/AbstractUserController.java
@@ -0,0 +1,49 @@
+package com.mytt.authorizationservice.web.user;
+
+import com.mytt.authorizationservice.error.IllegalRequestDataException;
+import com.mytt.authorizationservice.model.User;
+import com.mytt.authorizationservice.repository.UserRepository;
+import com.mytt.authorizationservice.util.UserUtil;
+import com.mytt.authorizationservice.web.JwtUser;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public abstract class AbstractUserController {
+
+    protected final Logger log = getLogger(getClass());
+
+    @Autowired
+    protected UserRepository repository;
+
+    @Autowired
+    private UniqueMailValidator emailValidator;
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.addValidators(emailValidator);
+    }
+
+    public ResponseEntity<User> get(int id) {
+        log.info("get {}", id);
+        return ResponseEntity.of(repository.findById(id));
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        repository.deleteExisted(id);
+    }
+
+    protected User prepareAndSave(User user) {
+        return repository.save(UserUtil.prepareToSave(user));
+    }
+
+    protected User findByJwtUser(JwtUser jwtUser) {
+        return repository.findById(jwtUser.id()).orElseThrow(
+                () -> new IllegalRequestDataException("User id='" + jwtUser.getId() + "' was not found"));
+    }
+}
