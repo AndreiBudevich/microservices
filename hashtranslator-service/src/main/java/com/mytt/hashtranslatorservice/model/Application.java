318ef96b30ff5187194939ea8ca52c2d70576372
Andrei Budevich <budevichac01@gmail.com>
2023-01-19 23:01:46 +0300

    create Application

---
+++ hashtranslator-service/src/main/java/com/mytt/hashtranslatorservice/model/Application.java
@@ -0,0 +1,24 @@
+package com.mytt.hashtranslatorservice.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "applications")
+@Getter
+@Setter
+public class Application {
+
+    @Id
+    private int id;
+
+    private List<String> hashes;
+
+    public Application(int id, List<String> hashes) {
+        this.id = id;
+        this.hashes = hashes;
+    }
+}
