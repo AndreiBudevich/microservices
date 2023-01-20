5b5a6dcc0609d5e68f7c64ddbd3d52789e5e0201
Andrei Budevich <budevichac01@gmail.com>
2023-01-20 11:14:11 +0300

    removed #addUserDetails

--- common/src/main/java/com/mytt/common/model/util/util/JwtUtil.java
+++ common/src/main/java/com/mytt/common/model/util/util/JwtUtil.java
@@ -12,15 +12,6 @@ import java.util.stream.Collectors;
 @Slf4j
 public class JwtUtil {
 
-    public static void addUserDetails(JwtClaimsSet.Builder builder, AuthUser authUser) {
-        User user = authUser.getUser();
-        String roles = user.getRoles().stream()
-                .map(Enum::name).collect(Collectors.joining(" "));
-        builder.subject(user.getEmail())
-                .claim("id", user.id())
-                .claim("roles", roles);
-    }
-
     public static JwtUser createJwtUser(Jwt jwt) {
         log.debug("create JwtUser for '{}'", jwt.getSubject());
         int id = ((Long) jwt.getClaims().get("id")).intValue();
